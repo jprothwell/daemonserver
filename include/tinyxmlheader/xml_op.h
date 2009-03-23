b24b6ec30427778c3b7cb609a9a9db2609a44eb8
sgq116300 <sgq116300@5edaaf0a-1527-11de-9433-ad2e3fad96c5>
2009-03-23 09:42:54 +0000

    xml changed done

--- include/tinyxmlheader/xml_op.h
+++ include/tinyxmlheader/xml_op.h
@@ -2,24 +2,31 @@
 #define _XML_OP_H_
 
 #include <memory>
+#include <string>
+
+using namespace std;
 
 class TiXmlDocument;
 
 enum XML_Status
 {
-    PARSE_DONE = 0x01,
-    PARSE_FAILED = 0x02,
-    ELEMENT_OK = 0x04,
-    ELEMENT_NOT_EXIST = 0x08,
-    ATTR_OK = 0x10,
-    ATTR_NOT_EXIST = 0x20
+    PARSE_DONE = 0x0001,
+    PARSE_FAILED = 0x0002,
+    ELEMENT_OK = 0x0004,
+    ELEMENT_NOT_EXIST = 0x0008,
+    ATTR_OK = 0x0010,
+    ATTR_NOT_EXIST = 0x0020,
+	ATTR_ADD_OK = 0x0040,
+	ATTR_ADD_FAILED = 0x0080,
+	ADD_TEXT_OK = 0x0100,
+	ADD_TEXT_FAILED = 0x0200
 };
 
 class XML_op
 {
 public:
     explicit XML_op( const string &filename );
-    explicit XML_op( const char* input );
+    //explicit XML_op( const char* input );
     XML_op();
     ~XML_op();
 
@@ -30,11 +37,11 @@ public:
     bool addElm( const string &elm );
     bool addAttr( const string &elm, const string &attr, const int &value );
     bool addAttr( const string &elm, const string &attr, const double &value );
-    bool addAttr( const string &elm, const string &attr, const string &value );
+    //bool addAttr( const string &elm, const string &attr, const string &value );
 
     bool addText( const string &elm, const string &text );
 
-    bool dumpXMl( char* outPut, int Size=0 );
+    bool dumpXML( char* outPut, int Size=0 );
 
 private:
     auto_ptr<TiXmlDocument> pDoc;
--- src/tinyxml/xml_op.cpp
+++ src/tinyxml/xml_op.cpp
@@ -1,9 +1,22 @@
 #include "xml_op.h"
 #include "tinyxml.h"
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+//#define DEBUG_XML
+
+#ifdef DEBUG_XML
+#define LOGXML(msg) do{ cout<<msg<<endl; }while(0)
+#else
+#define LOGXML(msg)
+#endif
 
 typedef TiXmlNode Node;
 typedef TiXmlElement Element;
 typedef TiXmlDocument Document;
+typedef TiXmlText Text;
 
 const char* XML_TITAL = "<?xml version=\"1.0\" standalone=\"no\" ?>\n";
 
@@ -17,6 +30,7 @@ XML_op::XML_op()
     }
 }
 
+/*
 XML_op::XML_op( const char* input )
     : pDoc( new TiXmlDocument() )
 {
@@ -26,6 +40,7 @@ XML_op::XML_op( const char* input )
         mStatus = PARSE_DONE;
     }    
 }
+*/
 
 XML_op::XML_op( const string &filename )
     : pDoc( new TiXmlDocument(filename) )   
@@ -47,23 +62,27 @@ bool XML_op::getAttrValue( const string &elm, const string &attr, int &value )
     Node *pNode = pDoc->FirstChild( elm );
     if( NULL==pNode )
     {
-        mStatus |= ELEMENT_NOT_EXIST;
+        mStatus = ELEMENT_NOT_EXIST;
+		LOGXML("getAttrValue::get node failed");
         return false;
     }
     
     Element *pElm = pNode->ToElement();
     if( NULL==pElm )
     {
-        mStatus |= ELEMENT_NOT_EXIST;
+        mStatus = ELEMENT_NOT_EXIST;
+		LOGXML("getAttrValue::to element failed");
         return false;
     }
 
     int res = pElm->QueryIntAttribute( attr.c_str(), &value );
     if( TIXML_SUCCESS!=res )
     {
-        mStatus |= ATTR_NOT_EXIST;
+        mStatus = ATTR_NOT_EXIST;
+		LOGXML("getAttrValue::get int attr failed");
         return false;
     }
+	mStatus = ATTR_OK;
 
     return true;
 }
@@ -73,36 +92,161 @@ bool XML_op::getAttrValue( const string &elm, const string &attr, double &value 
     Node *pNode = pDoc->FirstChild( elm );
     if( NULL==pNode )
     {
-        mStatus |= ELEMENT_NOT_EXIST;
+        mStatus = ELEMENT_NOT_EXIST;
         return false;
     }
     
     Element *pElm = pNode->ToElement();
     if( NULL==pElm )
     {
-        mStatus |= ELEMENT_NOT_EXIST;
+        mStatus = ELEMENT_NOT_EXIST;
         return false;
     }
 
     int res = pElm->QueryDoubleAttribute( attr.c_str(), &value );
     if( TIXML_SUCCESS!=res )
     {
-        mStatus |= ATTR_NOT_EXIST;
+        mStatus = ATTR_NOT_EXIST;
         return false;
     }
+	mStatus = ATTR_OK;
 
     return true;
 }
 
 bool XML_op::addElm( const string &elm )
 {
+	if( NULL!=pDoc->FirstChild(elm) )
+	{
+		mStatus = ELEMENT_OK;
+		return true;
+	}
+
     Element Elm( elm );
     Node *pNode = pDoc->FirstChild();
     if( NULL==pNode )
     {
-        mStatus |= ELEMENT_NOT_EXIST;
+        mStatus = ELEMENT_NOT_EXIST;
         return false;
     }
 
     pDoc->InsertAfterChild( pNode, Elm );
+	mStatus = ELEMENT_OK;
+
+	return true;
+}
+
+bool XML_op::addAttr( const string &elm, const string &attr, const int &value )
+{
+	Node *pNode = pDoc->FirstChild( elm );
+	if( NULL!=pNode )
+	{
+		//the element is exist
+		Element *pElm = pNode->ToElement();
+		if( NULL!=pElm )
+		{
+			pElm->SetAttribute( attr.c_str(), value );
+			mStatus = ATTR_ADD_OK;
+			return true;
+		}
+		else
+		{
+			mStatus = ATTR_ADD_FAILED;
+		}
+	}
+	else
+	{
+		bool ret = addElm( elm );
+		if( true==ret )
+		{
+			return addAttr( elm, attr, value );
+		}
+		else
+		{
+			mStatus = ATTR_ADD_FAILED;
+		}
+	}
+
+	return false;
+}
+
+bool XML_op::addAttr( const string &elm, const string &attr, const double &value )
+{
+	Node *pNode = pDoc->FirstChild( elm );
+	if( NULL!=pNode )
+	{
+		//the element is exist
+		Element *pElm = pNode->ToElement();
+		if( NULL!=pElm )
+		{
+			pElm->SetDoubleAttribute( attr.c_str(), value );
+			mStatus = ATTR_ADD_OK;
+			return true;
+		}
+		else
+		{
+			mStatus = ATTR_ADD_FAILED;
+		}
+	}
+	else
+	{
+		bool ret = addElm( elm );
+		if( true==ret )
+		{
+			return addAttr( elm, attr, value );
+		}
+		else
+		{
+			mStatus = ATTR_ADD_FAILED;
+		}
+	}
+
+	return false;
+}
+
+bool XML_op::addText( const string &elm, const string &text )
+{
+	if( true==addElm(elm) )
+	{
+		Node *pNode = pDoc->FirstChild( elm );
+		if( NULL!=pNode )
+		{
+			Element *pElm = pNode->ToElement();
+			Text textInfo( text );
+			pElm->InsertEndChild( textInfo );
+			mStatus = ADD_TEXT_OK;
+
+			return true;
+		}
+	}
+
+	mStatus = ADD_TEXT_FAILED;
+
+	return false;
+
+}
+
+bool XML_op::dumpXML( char* outPut, int Size )
+{
+	FILE *pFile = fopen( "./tmpfile", "w" );
+	if( NULL==pFile )
+	{
+		return false;
+	}
+
+	pDoc->Print( pFile, 0 );
+	fclose( pFile );
+	fstream fopen( "./tmpfile", fstream::in );
+	fopen.seekg( 0, ios::end );
+	int length = fopen.tellg();
+	fopen.seekg( 0, ios::beg );
+
+	if( length>Size )
+	{
+		return false;
+	}
+
+	fopen.read( outPut, length );
+
+	return true;
 }
